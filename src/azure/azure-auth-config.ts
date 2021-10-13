ebe1085a7bce27cfe9146ff1dbf5588b7c008f8b
Chris Vettese <chris.vettese2@gmail.com>
2021-10-12 23:59:33 -0400

    Fixed azure config

--- src/azure/azure-auth-config.ts
+++ src/azure/azure-auth-config.ts
@@ -1,6 +1,6 @@
 export const config = {
   REACT_APP_AZURE_ACTIVE_DIRECTORY_APP_CLIENT_ID: "7158c3bc-c32d-463f-87b2-bf5a0cbf0cfe",
-  redirectUri: "http://localhost:3000"
+  redirectUri: "https://3001-black-alpaca-x6f5wu70.ws-us18.gitpod.io/"
 }
 
 export const msalConfig = {
