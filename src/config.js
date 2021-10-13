028d5248e9d600eb8ab51c70a2f3347607f7f0c5
Jack Clarke <jclar162@uottawa.ca>
2021-10-13 02:22:35 +0000

    add config

---
+++ src/config.js
@@ -0,0 +1,5 @@
+const config = {
+    REACT_APP_AZURE_ACTIVE_DIRECTORY_APP_CLIENT_ID: "7158c3bc-c32d-463f-87b2-bf5a0cbf0cfe",
+    redirectUri: "https://3001-black-alpaca-x6f5wu70.ws-us18.gitpod.io/"
+}
+exports.config = config;
