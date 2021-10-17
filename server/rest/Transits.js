ec7cfa9808b7808ddc574797bb690c6961ac9e05
Jack Clarke <jclar162@uottawa.ca>
2021-10-17 04:04:34 +0000

    do transit

--- server/rest/Transits.js
+++ server/rest/Transits.js
@@ -1,13 +1,36 @@
 import { Router } from "express";
-import { TransitMethod } from "../models/Transit.js";
+import Transit, { TransitMethod } from "../models/Transit.js";
 import User from "../models/UserModel.js";
 import assert from "../utils/dev.js";
 import { getAllBuilder } from "./UserRest.js";
 const transitRouter = Router();
 
 transitRouter.get("/methods", getAllBuilder(TransitMethod));
-transitRouter.post("", (req, res) => {
-    const { oid, transit_id, gas_emissions, frequency, distance } = req.body
+function checkValues(arr, res) {
+    try {
+        arr.forEach((value) => {
+            assert(value, 'missing value received')
+        })
+        return true;
+    } catch (error) {
+        res.send(400).message(error.message);
+        return false
+    }
+
+}
+
+transitRouter.post("/", (req, res) => {
+    const { oid, transits } = req.body;
+    if (checkValues([oid, transits], res)) {
+        try{
+            transits.forEach((transit) => Transit.create(transit));
+            res.send(200).message("success");
+        } catch (error) {
+            res.send(500).message(error.message);
+        }
+        
+    }
+
 })
 
 export default transitRouter;
