d5a94dcc55f8a879c132ff18040105fc5158e46d
Jack Clarke <jclar162@uottawa.ca>
2021-10-17 01:23:48 +0000

    add optional frontend

--- server/rest/HelloWorld.js
+++ server/rest/HelloWorld.js
@@ -1,9 +1,21 @@
 import { Router } from "express";
 import * as express from "express"
 import { getFilePath } from "../utils/utils.js";
+import fs from 'fs'
 
 
 const router = Router();
-router.use('/', express.static(getFilePath(import.meta.url, '../static/build'), ))
+const frontendPath = getFilePath(import.meta.url, '../static/build');
+
+if (fs.existsSync(frontendPath)) {
+    console.log("static frontend found")
+    router.use('/', express.static(frontendPath));
+} else {
+    console.log("static frontend not found")
+}
+
+
+
+
 
 export default router;
